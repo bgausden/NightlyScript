6fc78ee9a6b30831844a1c33a5b99196176285d4
Orc <orc@linuxdev1>
2010-04-23 15:07:49 +0800

    Change default source to scp.orcsoftware.com (from storage.orcsoftware.com)

--- orc-nightly.sh
+++ orc-nightly.sh
@@ -41,7 +41,7 @@ SPARC="SPARC"
 SYSTEM=$(uname -s | tr "[:lower:]" "[:upper:]")	# e.g SunOS, Linux, Darwin -> SUNOS, LINUX, DARWIN
 ISA=$(uname -p | tr "[:lower:]" "[:upper:]") # e.g. sparc, x86_64, i386 -> SPARC, X86_64, I386
 
-DEFAULT_SOURCE_HOST=storage.orcsoftware.com #Default server to download from
+DEFAULT_SOURCE_HOST=scp.orcsoftware.com #Default server to download from
 ROOT_DIR="/pub/static/common/applications/orc" # Need this created on the source machine if doesn't exist.
 DEFAULT_BUILD="TS-9" # What to download if the user doesn't explictly choose a build to retrieve
 DEFAULT_LATEST_SUCCESS="S" # Download last available (irrespective of whether a complete build) or the last known successful build

ee01a1e35383524557117bfe9a777739ca0b59d2
Barry Gausden <barry@orc-group.com>
2012-08-27 14:17:30 +0800

    Update to reflect current naming of GW releases i.e. GW-*.
    Ensures that attempting to download e.g. GW-1-69 will not result in the destination directory being constructed as "Orc-GW-1-69" but rather GW-1-69.

--- orc-nightly.sh
+++ orc-nightly.sh
@@ -381,7 +381,7 @@ set_path()
 	SOURCE=${ROOT_DIR}
 
 	if [ ${SYSTEM} = ${DARWIN} ] ; then 								# MacOSX only
-		if [ ${DOWNLOAD_BUILD} = "HEAD" ] || [[ ${DOWNLOAD_BUILD} =~ TS-* ]] || [ ${DOWNLOAD_BUILD} = "GW" ] ; then	# non-numeric releases don't get the Orc- prefix
+		if [ ${DOWNLOAD_BUILD} = "HEAD" ] || [[ ${DOWNLOAD_BUILD} =~ TS-* ]] || [[ ${DOWNLOAD_BUILD} =~ GW-* ]] ; then	# non-numeric releases don't get the Orc- prefix
 			DEST_DIR="/Applications/Orc/"${DOWNLOAD_BUILD}
 		else
 			DEST_DIR="/Applications/Orc/Orc-"${DOWNLOAD_BUILD}																					# numeric releases do get the Orc- prefix
@@ -394,7 +394,7 @@ set_path()
 	fi
 	#TODO Fix this so it works for all users
 	if [ ${SYSTEM} = ${WINDOWS} ] ; then 								# Win only
-		if [ ${DOWNLOAD_BUILD} = "HEAD" ] || [[ ${DOWNLOAD_BUILD} =~ TS-* ]] || [ ${DOWNLOAD_BUILD} = "GW" ] ; then	# non-numeric releases don't get the Orc- prefix
+		if [ ${DOWNLOAD_BUILD} = "HEAD" ] || [[ ${DOWNLOAD_BUILD} =~ TS-* ]] || [[ ${DOWNLOAD_BUILD} =~ GW-* ]] ; then	# non-numeric releases don't get the Orc- prefix
 			DEST_DIR="${USERPROFILE}\"/Orc/"${DOWNLOAD_BUILD}\"
 		else
 			DEST_DIR="\"/cygdrive/c/Users/jeanm/Orc/Orc-"${DOWNLOAD_BUILD}\"																					# numeric releases do get the Orc- prefix
